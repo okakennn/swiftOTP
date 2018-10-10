b09c2869502ffb50e999097cdd2e236dec35b28d
okai <okai@iwmbp2014.local>
2018-10-10 13:09:50 +0900

    Initial Commit

---
+++ OTP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OTP
+//
+//  Created by okai on 2018/10/10.
+//  Copyright © 2018 okai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
